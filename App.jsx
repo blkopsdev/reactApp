4fef12a7bdeb269751d2dd2e81e4e1e7510bea4f
doctordev <doctordev0613@gmail.com>
2017-12-19 11:37:44 -0500

    Added simple component

--- App.jsx
+++ App.jsx
@@ -2,16 +2,34 @@ import React from 'react';
 
 class App extends React.Component {
     render() {
-        var i = 1;
-        var myStyle = {
-            fontSize: 100,
-            color: '#FF0000'
-        }
         return (
             <div>
-                <h1 className="header" style={myStyle}>{i == 1 ? 'True!' : 'False'}</h1>
+                <Header/>
+                <Content/>
             </div>
         );
     }
 }
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Header</h1>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Content</h2>
+                <p>The content text!!!</p>
+            </div>
+        );
+    }
+}
+
 export default App;
