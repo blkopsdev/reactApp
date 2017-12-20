bbfb9f8185823b4d69cd912e0b9c51d4f2ebee1c
doctordev <doctordev0613@gmail.com>
2017-12-20 01:12:38 -0200

    added proptype

--- App.jsx
+++ App.jsx
@@ -1,66 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
-
-    constructor() {
-        super();
-
-        this.state = {
-            data:
-            [
-                {
-                    "id" : 1,
-                    "name" : "Foo",
-                    "age" : "20"
-                },
-                {
-                    "id" : 2,
-                    "name" : "Bar",
-                    "age" : "30"
-                },
-                {
-                    "id" : 3,
-                    "name" : "Baz",
-                    "age" : "40"
-                }
-            ]
-        }
-    }
-
+    
     render() {
         return (
             <div>
-                <Header/>
-                <table>
-                    <tbody>
-                        {this.state.data.map((person, i) => <TableRow key = {i} data= {person} />)}
-                    </tbody>
-                </table>
+                <h3>Array: {this.props.propArray}</h3>
+                <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
+                <h3>Func: {this.props.propFunc(3)}</h3>
+                <h3>Number: {this.props.propNumber}</h3>
+                <h3>String: {this.props.propString}</h3>
+                <h3>Object: {this.props.propObject.objectName1}</h3>
+                <h3>Object: {this.props.propObject.objectName2}</h3>
+                <h3>Object: {this.props.propObject.objectName3}</h3>
             </div>
         );
     }
 }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-            </div>
-        );
-    }
+App.propTypes = {
+    propArray: React.PropTypes.array.isRequired,
+    propBool: React.PropTypes.bool.isRequired,
+    propFunc: React.PropTypes.func,
+    propNumber: React.PropTypes.number,
+    propString: React.PropTypes.string,
+    propObject: React.PropTypes.object,
 }
 
-class TableRow extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td>{this.props.data.id}</td>
-                <td>{this.props.data.name}</td>
-                <td>{this.props.data.age}</td>
-            </tr>
-        );
+App.defaultProps = {
+    propArray: [1,2,3,4,5],
+    propBool: true,
+    propFunc: function(e){return e},
+    propNumber: 1,
+    propString: "String value...",
+    propObject: {
+        objectName1: "objectValue1",
+        objectName2: "objectValue2",
+        objectName3: "objectValue3"
     }
 }
-
 export default App;
